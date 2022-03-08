d01d5289345be49e3ba530f56fb3f12bbde413ab
YASWANTHYASH6848 <99253813+YASWANTHYASH6848@users.noreply.github.com>
2022-03-08 11:04:07 +0530

    Add files via upload

---
+++ JAVA/JAVA11/Q1.java
@@ -0,0 +1,16 @@
+
+interface SimpleInterest {
+	
+	double SI(double P, double R, double T);
+
+}
+
+public class Q1 {
+
+    public static void main( String[] args ) {
+    SimpleInterest ref;
+    ref = (double P, double R, double T) -> {return (P*R*T)/100;};
+    
+    System.out.println("Simple Interest = " + ref.SI(100,15,3));
+    } 
+}
