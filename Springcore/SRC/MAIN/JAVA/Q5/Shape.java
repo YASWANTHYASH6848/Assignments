a910494e5a670d7d0cb53d76ac500f5585051f9e
YASWANTHYASH6848 <99253813+YASWANTHYASH6848@users.noreply.github.com>
2022-03-19 11:41:33 +0530

    Add files via upload

---
+++ Springcore/SRC/MAIN/JAVA/Q5/Shape.java
@@ -0,0 +1,20 @@
+package Q5;
+
+public interface Shape {
+	
+	public abstract void draw() ;
+
+}
+
+class injectimpl{
+	int s;
+	public int getS() {
+		return s;
+	}
+	public void setS(int s) {
+		this.s = s;
+	}
+	public String toString() {
+        return ("@injectImplementation"+s);
+    }
+}
