c46df588f8a9a53d5fa41e65c106c0b3d42237dd
YASWANTHYASH6848 <99253813+YASWANTHYASH6848@users.noreply.github.com>
2022-03-29 12:09:37 +0530

    Add files via upload

---
+++ Springsecurity/springsecurity-assignmentQ3/src/test/java/com/springSecurity/SpringsecurityAssignmentQ3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityAssignmentQ3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
