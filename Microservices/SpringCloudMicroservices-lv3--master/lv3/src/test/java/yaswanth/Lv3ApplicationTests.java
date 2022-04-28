9f5f0cb953f3953f7de28cfd6b71df3b820ffc4d
YASWANTHYASH6848 <99253813+YASWANTHYASH6848@users.noreply.github.com>
2022-04-28 08:47:45 +0530

    Add files via upload

---
+++ Microservices/SpringCloudMicroservices-lv3--master/lv3/src/test/java/yaswanth/Lv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package yaswanth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
